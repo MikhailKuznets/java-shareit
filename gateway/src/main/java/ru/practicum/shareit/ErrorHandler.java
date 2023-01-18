1fd5860ac5ec37aa15ea2af45f1f1470ad616318
KuznetsovMikhail <k-u-z-n-e-c@yandex.ru>
2023-01-18 19:45:12 +0300

    style: поправить CodeStyle

--- gateway/src/main/java/ru/practicum/shareit/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/ErrorHandler.java
@@ -18,9 +18,11 @@ public class ErrorHandler {
 
     public class ErrorResponse {
         private final String error;
+
         public ErrorResponse(String error) {
             this.error = error;
         }
+
         public String getError() {
             return error;
         }
